1e38472d4429e48d87cd16d4cd405313761786ab
Sol Grassi <grassisolgianna@gmail.com>
2025-03-21 10:53:27 -0300

    correcciones

--- Desafio Peliculas/funciones.py
+++ Desafio Peliculas/funciones.py
@@ -5,7 +5,7 @@ def input_movies():
     minutes = int(input(
         "Ingresá la duración de una película  (0 para finalizar): "
     ))
-    movies_duration  = []
+    movies_duration = []
     while minutes != 0:
         movies_duration .append(minutes)
         minutes = int(input(
@@ -21,7 +21,7 @@ def average_calculation(movies_duration):
     """
 
     len_movies = len(movies_duration)
-    average = float(0 if len_movies == 0 else sum(movies_duration) / len_movies)   
+    average = float(0 if len_movies == 0 else sum(movies_duration) / len_movies)
     return average
 
 
@@ -32,9 +32,9 @@ def max_average (movies_duration,average):
     """
     maax = [n for n in movies_duration if n > average]
     print(f"{len(maax)} películas duran más que el promedio ({average} min).")
+    
 
 if (__name__ == "__main__"):
-    # Invoco la función para cargar la duración de las películas
     movies = []
     movies = input_movies()
     # Invoco la función para calcular el promedio
